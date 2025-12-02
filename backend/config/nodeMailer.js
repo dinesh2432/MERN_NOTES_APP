f945d87271a5d2ada334bebd461eef68a3c3f23d
dinesh2432 <ddk113311@gmail.com>
2025-12-02 23:19:58 +0530

    updated nodemailer.js

--- backend/config/nodeMailer.js
+++ backend/config/nodeMailer.js
@@ -1,10 +1,12 @@
 const nodemailer = require('nodemailer')
 require('dotenv').config()
 const transporter = nodemailer.createTransport({
-  service:'gmail',
+  host: "smtp.ethereal.email",
+  port: 587,
+  secure: false, 
   auth: {
-    user: process.env.SENDER_MAIL,
-    pass: process.env.SENDER_APP_PASS,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD
   },
 });
 
