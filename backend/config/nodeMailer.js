230259635405d33c91340a1821991c56c2c229ce
dinesh2432 <ddk113311@gmail.com>
2025-12-03 15:26:34 +0530

    updated nodeMailer.js

--- backend/config/nodeMailer.js
+++ backend/config/nodeMailer.js
@@ -1,12 +1,13 @@
 const nodemailer = require('nodemailer')
 require('dotenv').config()
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
+  service:"gmail",
+  host: "smtp.gmail.com",
   port: 587,
   secure: false, 
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD
+    user: process.env.SENDER_MAIL,
+    pass: process.env.SENDER_APP_PASS
   },
 });
 
